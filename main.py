bdb17d507ddde11fcbbd47ed6eae4d7cfb88703a
vb690 <vb690@york.ac.uk>
2020-11-01 11:03:09 +0000

    Added requirements

--- main.py
+++ main.py
@@ -3,33 +3,33 @@ from modules.utils import load_data, comparison_pipeline
 DATA = {
     'mn_v': {
         'type': 'clas',
-        'data': load_data('mn_v', portion=0.33),
-        'min_delta': 0.0001,
+        'data': load_data('mn_v', portion=0.66),
+        'min_delta': 0.001,
         'n_classes': 10
     },
     'mn_b': {
         'type': 'clas',
-        'data': load_data('mn_b', portion=0.33),
-        'min_delta': 0.0001,
+        'data': load_data('mn_b', portion=0.66),
+        'min_delta': 0.001,
         'n_classes': 10
     },
     'mn_r': {
         'type': 'clas',
-        'data': load_data('mn_r', portion=0.33),
-        'min_delta': 0.0001,
+        'data': load_data('mn_r', portion=0.66),
+        'min_delta': 0.001,
         'n_classes': 10
     },
     'mn_rb': {
         'type': 'clas',
-        'data': load_data('mn_rb', portion=0.33),
-        'min_delta': 0.0001,
+        'data': load_data('mn_rb', portion=0.66),
+        'min_delta': 0.001,
         'n_classes': 10
     }
 
 }
 
-BUDGET = 33
-CV = 33
+BUDGET = 50
+CV = 50
 
 df = comparison_pipeline(
     data_sources=DATA,
